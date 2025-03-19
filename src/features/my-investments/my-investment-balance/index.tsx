8fc49bbf78e01b0f10b2866a6988da4d93f60989
sylvahh <sylvesterchambers03@gmail.com>
2025-03-19 12:19:28 +0100

    chore: made some changes on my investment

--- src/features/my-investments/my-investment-balance/index.tsx
+++ src/features/my-investments/my-investment-balance/index.tsx
@@ -5,18 +5,27 @@ import classNames from "classnames";
 import useBalance from "./use-balance";
 import ErrorBoundary from "@/components/error-boundary";
 import Render from "@/components/render";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default React.memo(function MyInvestmentBalance() {
   const { isFetching, isError, error, data, currency, sign, changeCurrency } =
     useBalance();
 
   const container = classNames(
-    "py-8 px-6 border rounded-lg shrink-0 lg:shrink w-full lg:w-1/2 min-h-48 lg:min-h-52",
+    "border rounded-lg shrink-0 lg:shrink w-full lg:w-1/2 min-h-48 lg:min-h-52",
+    {
+      "py-8 px-6": !isFetching
+    },
   );
   return (
     <ErrorBoundary>
       <div className={container}>
-        <Render isLoading={isFetching} isError={isError} error={error}>
+        <Render
+          isLoading={isFetching}
+          isError={isError}
+          error={error}
+          loadingComponent={<Skeleton className="h-48 lg:h-52" />}
+        >
           <div className="flex flex-col gap-6">
             <div className="flex justify-between">
               <div className="flex flex-col gap-5">
