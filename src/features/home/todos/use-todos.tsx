f787849e18a063afccc363c3895ced7fde3bfec2
sylvahh <sylvesterchambers03@gmail.com>
2025-04-11 12:40:28 +0100

    integration: home/todo made some changes on the todo file.

--- src/features/home/todos/use-todos.tsx
+++ src/features/home/todos/use-todos.tsx
@@ -43,7 +43,7 @@ export default function useTodos() {
     }
   }, [account.email]);
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     hasSecurityQuestions();
   }, [hasSecurityQuestions]);
 
