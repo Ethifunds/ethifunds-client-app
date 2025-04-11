80016c2a04a64acd814111642e31f87a84e5a60e
sylvahh <sylvesterchambers03@gmail.com>
2025-04-11 12:42:32 +0100

    refactor: remove unused transaction filters and update wallet transactions component

--- src/dialogs/wallet-dialog/fund-wallet-dialog/account-card.tsx
+++ src/dialogs/wallet-dialog/fund-wallet-dialog/account-card.tsx
@@ -1,46 +1,52 @@
 import { assets } from "@/constants";
-import { AccountWallet } from "@/types/wallet.types";
+import { VirtualAccount } from "@/types/wallet.types";
 
-export default function AccountCard(props: AccountWallet & { copy(text: string): Promise<void> }) {
-	const data = {
-		bank_name: props.bank_name,
-		bank_account_number: props.account_number,
-		beneficiary_name: props.account_name,
-	};
+export default function AccountCard(
+  props: VirtualAccount & { copy(text: string): Promise<void> },
+) {
+  const data = {
+    bank_name: props.bank_name,
+    bank_account_number: props.account_number,
+    beneficiary_name: props.account_name,
+  };
 
-	const copy = async () => {
-		props.copy(props.account_number);
-	};
+  const copy = async () => {
+    props.copy(props.account_number);
+  };
 
-	return (
-		<div className="flex flex-col gap-3 border rounded-2xl p-2">
-			{Object.entries(data).map(([key, value]) => {
-				const isAccountNumber = key === "bank_account_number" && true;
-				return (
-					<div
-						key={key}
-						className="flex justify-between items-center capitalize px-3 py-3 border-b last:border-b-0 [&_span]:w-full"
-					>
-						<span className="caption-standard  text-neutral-500">{key.split("_").join(" ")}:</span>
-						{isAccountNumber ? (
-							<div className="flex items-center gap-2 px-3">
-								<span className="content-accent text-neutral-1000">{value}</span>
-								<button
-									onClick={copy}
-									className="button-ghost !p-0 hover:scale-105 flex items-center gap-2"
-								>
-									<img src={assets.copy_icon_01} alt="copy-icon" />
-									<span>Copy</span>
-								</button>
-							</div>
-						) : (
-							<span className="content-accent text-right text-neutral-1000">
-								{value.toLocaleLowerCase()}
-							</span>
-						)}
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-3 rounded-2xl border p-2">
+      {Object.entries(data).map(([key, value]) => {
+        const isAccountNumber = key === "bank_account_number" && true;
+        return (
+          <div
+            key={key}
+            className="flex items-center justify-between border-b px-3 py-3 capitalize last:border-b-0 [&_span]:w-full"
+          >
+            <span className="caption-standard text-neutral-500">
+              {key.split("_").join(" ")}:
+            </span>
+            {isAccountNumber ? (
+              <div className="flex items-center gap-2 px-3">
+                <span className="content-accent text-neutral-1000">
+                  {value}
+                </span>
+                <button
+                  onClick={copy}
+                  className="button-ghost flex items-center gap-2 !p-0 hover:scale-105"
+                >
+                  <img src={assets.copy_icon_01} alt="copy-icon" />
+                  <span>Copy</span>
+                </button>
+              </div>
+            ) : (
+              <span className="content-accent text-right text-neutral-1000">
+                {value.toLocaleLowerCase()}
+              </span>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/dialogs/wallet-dialog/fund-wallet-dialog/use-fund-wallet.tsx
+++ src/dialogs/wallet-dialog/fund-wallet-dialog/use-fund-wallet.tsx
@@ -1,4 +1,4 @@
-import getAccountWallets from "@/services/wallet/get-account-wallets";
+import getVirtualAccount from "@/services/wallet/get-virtual-accounts";
 import useActions from "@/store/actions";
 import { useAppSelector } from "@/store/hooks";
 import * as React from "react";
@@ -6,86 +6,87 @@ import { useQuery } from "react-query";
 import { toast } from "sonner";
 
 export default function useFundWallet() {
-	const { currency } = useAppSelector((state) => state.account);
-	const { dialog } = useAppSelector((state) => state.ui);
-	const [copied, setCopied] = React.useState(false);
-	const [confirming, setConfirming] = React.useState(false);
-	const { ui } = useActions();
+  const { currency } = useAppSelector((state) => state.account);
+  const { dialog } = useAppSelector((state) => state.ui);
+  const [copied, setCopied] = React.useState(false);
+  const [confirming, setConfirming] = React.useState(false);
+  const { ui } = useActions();
 
-	const code = currency.code;
+  const code = currency.code;
 
-	const copiedRef = React.useRef<HTMLDivElement>(null);
+  const copiedRef = React.useRef<HTMLDivElement>(null);
 
-	const open = React.useMemo(() => {
+  const open = React.useMemo(() => {
     return dialog.show && dialog.type === "fund_wallet";
   }, [dialog.show, dialog.type]);
 
   const query = useQuery(
     ["account-wallets", code],
-    () => getAccountWallets({ currency: code }),
+    () => getVirtualAccount({ currency: code }),
     { enabled: open },
   );
 
-	const toggleShow = (val: boolean) => {
-		ui.changeDialog({ show: val, type: "", id: "" });
-	};
+  const toggleShow = (val: boolean) => {
+    ui.changeDialog({ show: val, type: "", id: "" });
+  };
 
+  React.useMemo(() => {
+    if (!copied) return;
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  }, [copied]);
 
-	React.useMemo(() => {
-		if (!copied) return;
-		setTimeout(() => {
-			setCopied(false);
-		}, 3000);
-	}, [copied]);
+  const scrollIntoView = () => {
+    if (copiedRef.current) {
+      copiedRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  };
 
-	const scrollIntoView = () => {
-		if (copiedRef.current) {
-			copiedRef.current.scrollIntoView({
-				behavior: "smooth",
-				block: "end",
-			});
-		}
-	};
+  const copy = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+      scrollIntoView();
+    } catch (err) {
+      toast.error("Failed to copy text");
+      console.error("Failed to copy text:", err);
+    }
+  };
 
-	const copy = async (text: string) => {
-		try {
-			await navigator.clipboard.writeText(text);
-			setCopied(true);
-			scrollIntoView();
-		} catch (err) {
-			toast.error("Failed to copy text");
-			console.error("Failed to copy text:", err);
-		}
-	};
+  // const showReceipt = () => {
+  // 	toggleShow(false);
+  // 	ui.changeDialog({
+  // 		type: "funding_receipt",
+  // 		id: "",
+  // 		show: true,
+  // 	});
+  // };
 
-	// const showReceipt = () => {
-	// 	toggleShow(false);
-	// 	ui.changeDialog({
-	// 		type: "funding_receipt",
-	// 		id: "",
-	// 		show: true,
-	// 	});
-	// };
+  const confirm = async () => {
+    setConfirming(true);
+    toggleShow(false);
+    toast.info(
+      "Confirming Payment, Amount would reflect on wallet once confirmed",
+      {
+        duration: 10000,
+        closeButton: true,
+      },
+    );
+    setConfirming(false);
+  };
 
-	const confirm = async () => {
-		setConfirming(true);
-		toggleShow(false);
-		toast.info("Confirming Payment, Amount would reflect on wallet once confirmed", {
-			duration: 10000,
-			closeButton: true,
-        });
-		setConfirming(false);
-        
-	};
-
-	return {
-		...query,
-		open,
-		copied,
-		copiedRef,
-		confirming,
-		copy,
-		toggleShow,
-		confirm,
-	};
+  return {
+    ...query,
+    open,
+    copied,
+    copiedRef,
+    confirming,
+    copy,
+    toggleShow,
+    confirm,
+  };
 }
